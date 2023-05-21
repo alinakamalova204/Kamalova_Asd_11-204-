c264edd9f0020ee8d3262fbebad9bfe7a684fb2b
alinakamalova204 <alinakamalova919@gmail.com>
2023-05-21 12:05:42 +0300

    алгоритм быстрого поиска

--- HomeWork2/src/Main.java
+++ HomeWork2/src/Main.java
@@ -4,43 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // {3,1} {1,2} {2,4} {5,4} {5,6} {6,7} {8,7} {9,8} {0,9} {9,7}
+        //алгоритм быстрого поиска
         Scanner scanner = new Scanner(System.in);
-        int [] arr = new int[10];
-        for (int i = 0; i < arr.length; i++) {
+        int n = scanner.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) { //массив [0,1,2,3,4,5,6,7,8,9]
             arr[i] = i;
         }
-        System.out.println(Arrays.toString(arr));
-
+        int p = scanner.nextInt();
+        int q = scanner.nextInt();
         for (int i = 0; i < arr.length; i++) {
-            int p = scanner.nextInt();
-            int q = scanner.nextInt();
             if (arr[p] != arr[q]){
-                if (p < q) {
-                    arr[q] = arr[p];
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[j] == arr[p]){
+                        arr[j] = arr[q];
+                    }
                 }
-                else {
-                    arr[p] = arr[q];
-                }
-                System.out.println(Arrays.toString(arr));
             }
-        }
-        System.out.println(Arrays.toString(arr));
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        boolean flag = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[a] == arr[b]){
-                flag = true;
-            }
-            else {
-                flag = false;
-            }
-        }
-        if (flag == false){
-            System.out.println("Не надо");
-        }else {
-            System.out.println("Надо");
         }
     }
 }
